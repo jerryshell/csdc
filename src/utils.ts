557ee3359a105b23a530c056d9be2d52403ec9cd
jerry <calljerryli@outlook.com>
2023-01-16 14:46:18 +0800

    线段交点工具函数

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,42 @@
+import ILine from './2d/ILine'
+
 const lerp = (a: number, b: number, t: number) => {
     return a + (b - a) * t
 }
 
+// 获得两条线段的交点
+const getIntersection = (
+    line1: ILine,
+    line2: ILine,
+): {
+    x: number,
+    y: number,
+    t: number,
+} | null => {
+    const tTop = (line2.endPoint.x - line2.startPoint.x) * (line1.startPoint.y - line2.startPoint.y) - (line2.endPoint.y - line2.startPoint.y) * (line1.startPoint.x - line2.startPoint.x)
+
+    const uTop = (line2.startPoint.y - line1.startPoint.y) * (line1.startPoint.x - line1.endPoint.x) - (line2.startPoint.x - line1.startPoint.x) * (line1.startPoint.y - line1.endPoint.y)
+
+    const bottom = (line2.endPoint.y - line2.startPoint.y) * (line1.endPoint.x - line1.startPoint.x) - (line2.endPoint.x - line2.startPoint.x) * (line1.endPoint.y - line1.startPoint.y)
+
+    if (bottom === 0) {
+        return null
+    }
+
+    const t = tTop / bottom
+    const u = uTop / bottom
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+        return {
+            x: lerp(line1.startPoint.x, line1.endPoint.x, t),
+            y: lerp(line1.startPoint.y, line1.endPoint.y, t),
+            t,
+        }
+    }
+
+    return null
+}
+
 export default {
     lerp,
+    getIntersection,
 }
